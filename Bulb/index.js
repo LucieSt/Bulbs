6ea35f78d07a084ff839b4b6963116e94b184504
LucieSt <strakova.91@gmail.com>
2022-10-25 21:25:10 +0200

    added Bulb file

---
+++ Bulb/index.js
@@ -0,0 +1,23 @@
+export const Bulb = (props) => {
+
+    let { isOn } = props;
+
+    const zarovka = document.createElement('div');
+    zarovka.classList.add('bulb', 'bulb--on');
+
+    console.log(isOn)
+
+    if (!isOn) {
+        zarovka.classList.remove('bulb--on');
+    }
+
+    zarovka.addEventListener('click', () => {
+        zarovka.replaceWith(
+            Bulb({
+                isOn: !isOn
+            })
+        )
+    })
+
+    return zarovka;
+}
